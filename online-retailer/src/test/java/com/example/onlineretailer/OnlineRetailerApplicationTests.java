398ab92bb6b5b4db0939064ce364ae47d5ea4b76
Victor <victor.cruz@rappi.com>
2022-07-16 12:21:37 -0500

    Init Online Retailer assigment

---
+++ online-retailer/src/test/java/com/example/onlineretailer/OnlineRetailerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineretailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineRetailerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
